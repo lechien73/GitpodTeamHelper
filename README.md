1b46b9989b49e7f3316a35253a3e8985938459bb
Matt Rudge <mrudge@gmail.com>
2021-07-06 17:35:51 +0100

    Update README.md and tweak code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# GitpodTeamHelper
+# Gitpod Team Helper Extension
+
+## What is it?
+
+A Chrome/Brave extension to make administering the Code Institute Gitpod team easier.
+
+Firstly, it adds text to the **Manage Teams** modal so that it lists our total active and total inactive users.
+
+Secondly, it highlights inactive users in green, making them easier to see as you scroll.
+
+## How do I install it?
+
+It's a bit specialised so it's not in the extension store. You'll need to sideload it.
+
+1. Download this repo locally and unzip it
+2. Go to the Extensions screen in Chrome or Brave
+3. Click on the *Developer Mode* button
+4. Click on *Load Unpacked*
+5. Load the extension from the folder you downloaded
+
+## Privacy & Security
+
+The extension only works on the gitpod.io/teams page. It only stores enabled/disabled data - nothing else. It's as safe as houses.
+
+## Todo
+
+I want to add the option to hide and reveal active users, which I think would make it more useful, but that's for a bit later.
+
+Icons, maybe...
+
+## Can I contribute?
+
+You betcha! Make your wondrous changes and submit a PR and I will be eternally grateful until I forget.
+
+------
+Matt Rudge<br />
+July 2021
---
+++ js/find_things.js
@@ -0,0 +1,65 @@
+(function() {
+
+    const targetNode = document.getElementById("root");
+    const config = { attributes: true, childList: true, subtree: true };
+    const searchString = "github.com/username";
+
+    let isDisabled = false;
+    let hasRun = false;
+    let hideActive = false;
+
+    const callback = function(mutationsList, observer) {
+        for (const mutation of mutationsList) {
+            if (mutation.type === "childList") {
+                var hasClass = [].some.call(mutation.addedNodes, function(el) {
+                    return "classList" in el ? el.classList.contains("pt-2") : null;
+                });
+                if (hasClass && !hasRun) {
+                    setCount();
+                    hasRun = true;
+                }
+            }
+        }
+    };
+
+    const observer = new MutationObserver(callback);
+
+    observer.observe(targetNode, config);
+
+
+    chrome.storage.sync.get('isDisabled', function(data) {
+        isDisabled = data.isDisabled;
+        console.log(isDisabled ? "GpTeamsAdmin disabled" : "GpTeamsAdmin active");
+    });
+
+    // Laters
+    // chrome.storage.sync.get('hideActive', function (data) {
+    //     hideActive = data.hideActive;
+    // });
+
+    function setCount() {
+        if (!isDisabled) {
+            let inactiveUserCount = 0;
+            let activeUserCount = 0;
+
+            let userTags = document.getElementsByTagName("input");
+
+            for (let tag of userTags) {
+                if (tag.getAttribute("placeholder").indexOf(searchString) > -1) {
+                    inactiveUserCount++;
+                    tag.style.cssText = "background-color: lightgreen!important;"
+                } else if (tag.getAttribute("placeholder").indexOf(searchString) === -1) {
+                    activeUserCount++;
+                    if (hideActive) {
+                        tag.parentElement.parentElement.style.cssText = "display: none!important;";
+                    }
+                }
+            }
+
+            let newDiv = document.getElementsByClassName("pb-2")[0].nextElementSibling;
+            newDiv.firstChild.innerHTML = `Add members using their username prefixed by the Git Provider's host. Currently, we have <strong>${activeUserCount}</strong> active users with <strong>${inactiveUserCount}</strong> inactive. Inactive users are highlighted in green.`
+        }
+    }
+
+
+})();
